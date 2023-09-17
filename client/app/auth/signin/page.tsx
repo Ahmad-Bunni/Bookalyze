1275cf34f88670d216268ab89cf429c1071ea554
DESKTOP-3E4KQ2J\Ahmad <Ahmad-Bunni@hotmail.com>
2023-09-18 00:00:32 +0200

    BK-8 test

--- client/app/auth/signin/page.tsx
+++ client/app/auth/signin/page.tsx
@@ -1,15 +1,9 @@
 import { Container } from '@/app/common/layout/Container'
-import { getProviders } from 'next-auth/react'
-import SignInLink from './components/SignInProvider'
 
-export default async function Page() {
-  const providers = await getProviders()
-  console.log(providers)
+export default function Page() {
+  // const providers = await getProviders()
 
-  if (!providers) {
-    console.log('empty')
-    return
-  }
+  // if (!providers) return
 
   return (
     <Container>
@@ -23,11 +17,11 @@ export default async function Page() {
           </p>
         </div>
 
-        <div className="flex w-full max-w-lg flex-col justify-end gap-4 p-4">
+        {/* <div className="flex w-full max-w-lg flex-col justify-end gap-4 p-4">
           {Object.values(providers).map((provider) => (
             <SignInLink key={provider.id} provider={provider} />
           ))}
-        </div>
+        </div> */}
       </div>
     </Container>
   )
