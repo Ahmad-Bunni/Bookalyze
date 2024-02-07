04c4e96539bc4aef7e5cb2b8bfbe65bca6917f82
DESKTOP-3E4KQ2J\Ahmad <Ahmad-Bunni@hotmail.com>
2024-02-07 23:49:19 +0100

    clean ups

--- server/app/chat/chat_service.py
+++ server/app/chat/chat_service.py
@@ -3,19 +3,36 @@ from typing import AsyncIterable, List
 
 from app.core.pinecone_service import PineconeService
 from langchain.memory import ChatMessageHistory, ConversationBufferMemory
-from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
+from prompts import coder_prompt
 
 
 class ChatService:
     def __init__(self, namespace, embeddings, index):
         self.pinecone_service = PineconeService(namespace, embeddings, index)
+        self.model = ChatOllama(model="deepseek-coder:6.7b")
 
     async def answer_question(self, messages: List[dict]) -> AsyncIterable[str]:
         input = messages.pop()["content"]
 
+        memory: ConversationBufferMemory = self.build_memory(messages)
+
+        chain = (
+            RunnablePassthrough.assign(
+                history=RunnableLambda(memory.load_memory_variables)
+                | itemgetter("history")
+            )
+            | coder_prompt
+            | self.model
+            | StrOutputParser()
+        )
+
+        async for chunk in chain.astream({"input": input}):
+            yield chunk
+
+    def build_memory(messages: List[dict]):
         chat_history = ChatMessageHistory()
 
         for m in messages:
@@ -24,31 +41,4 @@ class ChatService:
             else:
                 chat_history.add_ai_message(m["content"])
 
-        prompt = ChatPromptTemplate.from_messages(
-            [
-                ("system", "You are a helpful chatbot"),
-                MessagesPlaceholder(variable_name="history"),
-                ("human", "{input}"),
-            ]
-        )
-
-        memory = ConversationBufferMemory(
-            return_messages=True, chat_memory=chat_history
-        )
-
-        model = ChatOllama(
-            model="deepseek-coder:6.7b",
-        )
-
-        chain = (
-            RunnablePassthrough.assign(
-                history=RunnableLambda(memory.load_memory_variables)
-                | itemgetter("history")
-            )
-            | prompt
-            | model
-            | StrOutputParser()
-        )
-
-        async for chunk in chain.astream({"input": input}):
-            yield chunk
+        return ConversationBufferMemory(return_messages=True, chat_memory=chat_history)
---
+++ server/app/chat/prompts.py
@@ -0,0 +1,9 @@
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+coder_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful chatbot"),
+        MessagesPlaceholder(variable_name="history"),
+        ("human", "{input}"),
+    ]
+)
