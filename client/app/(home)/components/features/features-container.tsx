3c206c27ac815e114fb94b971261b2727fa3896c
DESKTOP-3E4KQ2J\Ahmad <Ahmad-Bunni@hotmail.com>
2024-03-09 22:26:29 +0100

    clean ups

--- client/app/(home)/components/features/features-container.tsx
+++ client/app/(home)/components/features/features-container.tsx
@@ -4,7 +4,9 @@ export function Features() {
   return (
     <div className="flex gap-6 border-y py-8 max-lg:flex-col">
       <FeatureItem title="Instant AI Insights" content="Upload your file and uncover real-time insights." />
+
       <FeatureItem title="Precision Filters" content="Enhance answers with optional page filters." />
+
       <FeatureItem title="Supported File Types" content=" • PDF • Word • CSV • TXT" />
     </div>
   )
--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -12,6 +12,7 @@ export default function Main() {
       </div>
 
       <Features />
+
       <PricingContainer />
     </div>
   )
