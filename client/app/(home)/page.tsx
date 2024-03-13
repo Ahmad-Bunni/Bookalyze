41962106392bdaf83fd40fd42a099eef83a73db7
DESKTOP-3E4KQ2J\Ahmad <Ahmad-Bunni@hotmail.com>
2024-03-13 23:04:15 +0100

    remove unused icons

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -7,7 +7,6 @@ export default function Main() {
       <div className="mx-auto text-center">
         <h1 className="text-2xl font-bold lg:text-5xl">
           <p>Dive Deeper with AI</p>
-          <p>Unearth Your File Insights</p>
         </h1>
       </div>
 
--- client/app/common/Icons/ChatIcon.tsx
+++
@@ -1,8 +0,0 @@
-export const ChatIcon = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" className="w-8" viewBox="0 0 1024 1024">
-    <path
-      fill="currentColor"
-      d="M296.832 672H832V256H192v503.36L296.832 672zM159.488 869.76A19.2 19.2 0 0 1 128 855.04V256a64 64 0 0 1 64-64h640a64 64 0 0 1 64 64v416a64 64 0 0 1-64 64H320L159.488 869.76z"
-    />
-  </svg>
-)
